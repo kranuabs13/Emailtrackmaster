21972b5fe99fc86286f136b5f7c79c2925a0fbdb
kranuabs13 <send.david@gmail.com>
2026-02-27 23:31:12 +0200

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const SUPABASE_URL = "https://glastmgtyigzbxxgcdbo.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_jmM4i9pwWtc8J8WvCiCp3Q_wfM7ncvz";
+const SUPABASE_URL = "PASTE_YOUR_PROJECT_URL_HERE"
+const SUPABASE_ANON_KEY = "PASTE_YOUR_ANON_KEY_HERE"
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
